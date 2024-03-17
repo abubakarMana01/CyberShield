971c979fcd49d5a6b2019fe0fc2e5db5ca71f31b
abubakarMana <ym.abubakr7@gmail.com>
2024-03-17 19:22:35 +0100

    fix

--- src/screens/auth/Biometrics.tsx
+++ src/screens/auth/Biometrics.tsx
@@ -1,17 +1,48 @@
-import {StyleSheet} from 'react-native';
+import {Alert, StyleSheet, TouchableOpacity, View} from 'react-native';
 import React from 'react';
+import TouchID from 'react-native-touch-id';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import {AppButton, AuthHeaderLogo, Screen} from '../../components';
+import {AppButton, AuthHeaderLogo, Screen, Text} from '../../components';
+import {COLORS} from '../../constants/colors';
+import {useAppContext} from '../../contexts/AppProvider';
 
 const Biometrics = () => {
+  const {setUser} = useAppContext();
+
+  const authenticate = () => {
+    TouchID.authenticate('to demo this react-native component')
+      .then((success: any) => {
+        console.log('Success', success);
+      })
+      .catch((error: any) => {
+        Alert.alert(
+          'Failed',
+          'Ensure device has biometrics authentication set up.',
+        );
+        console.log(error);
+      });
+  };
+
   return (
     <Screen>
       <AuthHeaderLogo />
 
+      <View style={styles.container}>
+        <Text style={styles.authText}>Touch to authenticate</Text>
+        <TouchableOpacity onPress={authenticate}>
+          <MaterialCommunityIcons
+            name="fingerprint"
+            size={60}
+            color={COLORS.black}
+          />
+        </TouchableOpacity>
+      </View>
+
       <AppButton
         text="Proceed"
         // isLoading={isLoading}
-        handleClick={() => {}}
+        handleClick={() => setUser({})}
         containerStyles={styles.ctaButton}
       />
     </Screen>
@@ -21,7 +52,11 @@ const Biometrics = () => {
 export default Biometrics;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   image: {
     width: 200,
     height: 200,
@@ -30,6 +65,13 @@ const styles = StyleSheet.create({
   },
   ctaButton: {
     marginTop: 'auto',
+    marginBottom: 16,
+  },
+  authText: {
+    textAlign: 'center',
+    marginTop: -100,
+    fontSize: 18,
+    fontWeight: '500',
     marginBottom: 16,
   },
 });
